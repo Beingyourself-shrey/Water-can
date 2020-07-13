993ec39a7030e080e48bc5c31f96c345871d3a86
shrey <crazymeshrey@gmail.com>
2020-07-13 18:53:46 +0530

    1. Message service added 2. Halt screen made in case app don't want to run

--- app/src/main/java/com/qapaper/myapplication/OrderConfirmActivity.java
+++ app/src/main/java/com/qapaper/myapplication/OrderConfirmActivity.java
@@ -2,9 +2,11 @@ package com.qapaper.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,12 +18,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 
 public class OrderConfirmActivity extends AppCompatActivity {
     SharedPreferences spref;
     SharedPreferences.Editor editor;
     DatabaseReference dref;
+    String msg;
     RelativeLayout rl;
     TextView location_tv,address_tv,type_tv,quantity_tv,price_tv,total_tv,phone_tv,email_tv;
     String user;
@@ -74,6 +81,7 @@ public class OrderConfirmActivity extends AppCompatActivity {
         String location=spref.getString("location","");
         String address=spref.getString("address","");
         Date date = new Date();
+         msg="Username: "+username+"\n Phone: "+phone+"\nEmail: "+email+"\nType: "+type+"\nPrice: "+price+"\nTotal Price: "+total_price+"\nQuantity: "+quantity+"\nLocation: "+location+"\nAddress: "+address+"\nDate: "+date;
 
 
         //setting all sharepref values to shareprefDetailsClass
@@ -103,6 +111,44 @@ public class OrderConfirmActivity extends AppCompatActivity {
                 editor=spref.edit();
                 editor.putString("quantity","1");
                 editor.apply();
+
+                //sms api
+
+
+
+                try {
+                    // Construct data
+                    String apiKey = "apikey=" + "MRKFXNEiepA-t9BFAiMVRfXMkrmMXALXuhAH9LQzGF";
+                    String message = "&message=" +msg;
+                    String sender = "&sender=" + "TXTLCL";
+                    String numbers = "&numbers=" + "918871009875";
+
+                    // Send data
+                    HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
+                    String data = apiKey + numbers + message + sender;
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+                    conn.getOutputStream().write(data.getBytes("UTF-8"));
+                    final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    final StringBuffer stringBuffer = new StringBuffer();
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        stringBuffer.append(line);
+                        Log.d("messagesbyshrey", line);
+                    }
+                    rd.close();
+
+
+                } catch (Exception e) {
+                    Log.d("Error SMS ", e.toString());
+
+                }
+
+
+
+                //sms api end
+
                 Intent i=new Intent(getApplicationContext(),Location.class);
                 startActivity(i);
                 finish();
@@ -115,6 +161,13 @@ public class OrderConfirmActivity extends AppCompatActivity {
 
             }
         });
+
+        StrictMode.ThreadPolicy p= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(p);
+
+
+
+
     }
     public void myinfo(View view) {
         Intent i=new Intent(this,User.class);
--- app/src/main/java/com/qapaper/myapplication/welcome.java
+++ app/src/main/java/com/qapaper/myapplication/welcome.java
@@ -3,6 +3,7 @@ package com.qapaper.myapplication;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,14 +13,39 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import com.google.firebase.FirebaseAppLifecycleListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.EventListener;
+
 import static android.support.v4.content.ContextCompat.startActivity;
 
 public class welcome extends AppCompatActivity {
-
+DatabaseReference dref;
+    Boolean value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        dref= FirebaseDatabase.getInstance().getReference().child("halt");
+        dref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                value=Boolean.parseBoolean(dataSnapshot.child("value").getValue().toString());
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -40,9 +66,15 @@ public class welcome extends AppCompatActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-            Intent i = new Intent(welcome.this, Login.class);
+            if(value)
+           {
+            Intent i = new Intent(welcome.this, Halt.class);
             startActivity(i);
+            }
+            else{
+                Intent i = new Intent(welcome.this, Login.class);
+                startActivity(i);
+            }
             welcome.this.finish();
 
         }
